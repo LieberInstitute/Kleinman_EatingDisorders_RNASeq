7c2a5a4d1106e9411c00473214fe187f0e333446
Chao Li <chao.li@libd.org>
2023-07-26 11:38:03 -0400

    Update DE_subtype_AN.R

--- DE_subtype_AN.R
+++ DE_subtype_AN.R
@@ -1,4 +1,12 @@
+library(jaffelab)
+library(SummarizedExperiment)
+library(sva)
+library(readxl)
+library(devtools)
+
+# load rse_gene data in AMD
 load('expr_cutoff/rse_gene.Rdata')
+
 # read in DSM4 Eating disorder subtype information from Dropbox
 subtype=read.table("ED_subtype.txt",header=TRUE)
 colData(rse_gene)$ED.subtype=subtype$subtype[match(rse_gene$BrNum,subtype$BrNum)]
