77a19914cce374a9db54a4e72a48166624ee9a77
Chao Li <chao.li@libd.org>
2023-08-16 13:32:00 -0400

    Update 20230815_PRS_correlation.R

--- 20230815_PRS_correlation.R
+++ 20230815_PRS_correlation.R
@@ -1,3 +1,8 @@
+library(enrichplot)
+library(DOSE)
+library(data.table)
+library(magrittr)
+
 ## /home/data1/R/prs/ed_n127
 files=list.files(pattern=".profile$")
 files=files[8:14]
@@ -23,7 +28,12 @@ load("/home/data1/R/idcheck/refgeno_n2630_20230522.rda")
 rm(refgeno)
 
 pr.score$BrNum=brains$BrNumCor[match(rownames(pr.score),brains$ID)]
-
+## read in PCs
+pcs <- fread("n127.eigenvec", header=F) %>%
+    setnames(., colnames(.), c("FID", "IID", paste0("PC",1:20)) )
+identical(rownames(pr.score),pcs$IID)
+#[1] TRUE
+pr.score=cbind(pr.score,pcs[,3:22]
 save(pr.score,file='n125_prs.rda')
 
 
@@ -40,6 +50,7 @@ prs$group=sapply(prs$Group,unclass)
 
 
 library(psych)
+library(writexl)
 data=data.frame(gene=exp[1,],prs1=prs[,1],age=rse_gene$AgeDeath,group=prs$group)
 result=corr.test(x=data,y=NULL,use='pairwise',method='pearson',adjust='holm',alpha=0.5,ci=TRUE)
 # x could be a matrix/dataframe of expression
@@ -51,7 +62,7 @@ result=corr.test(x=data,y=NULL,use='pairwise',method='pearson',adjust='holm',alp
 
 rownames(prs)=rse_gene$BrNum
 prs.res=sapply(prs[,1:7],function(x){
-  fit=lm(x~rse_gene$AgeDeath+group+mds$snpPC1+mds$snpPC2+mds$snpPC3+mds$snpPC4+mds$snpPC5,data=prs)
+  fit=lm(x~rse_gene$AgeDeath+group+PC1+PC2+PC3+PC4+PC5,data=prs)
   return(residuals(fit))
 })
 
@@ -59,7 +70,7 @@ dim(prs.res)
 # [1] 125   7
 
 exp=exp[,-c(5,57)]
-COR=corr.test(x=t(exp),y=prs.res,use='pairwise',method='pearson',adjust='holm',alpha=0.5,ci=TRUE)
+# COR=corr.test(x=t(exp),y=prs.res,use='pairwise',method='pearson',adjust='holm',alpha=0.5,ci=TRUE)
 # take long time
 
 library(parallel)
@@ -67,27 +78,24 @@ COR.list=mclapply(rownames(exp),function(x) {
 	result=corr.test(x=exp[x,],y=prs.res,use='pairwise',method='pearson',adjust='holm',alpha=0.5,ci=TRUE)
 	return(result)
 },mc.cores=8)
-COR.3snpPC=sapply(COR.list,function(x){
+COR=sapply(COR.list,function(x){
  return(x$p)
  })
-COR.3snpPC=t(COR.3snpPC)
-colnames(COR.3snpPC)=c('0.001','0.05','0.1','0.2','0.3','0.4','0.5')
-rownames(COR.3snpPC)=rownames(exp)
+COR=t(COR)
+colnames(COR)=c('0.001','0.05','0.1','0.2','0.3','0.4','0.5')
+rownames(COR)=rownames(exp)
 
-which(COR.3snpPC==min(COR.3snpPC))%%25148
-COR.3snpPC[22693,] # is the mininal exp
-
-save(COR,exp,prs,prs.res,file='/home/data1/R/prs/ed_n127/n125_pearsonCor_result.rda')
-
-x=COR$p
-min(x)
-# [1] 3.113981e-05
-head(x[order(x[,3]),])
-#                        0.001         0.05          0.1          0.2
-# ENSG00000230673.3  0.27920174 7.689147e-04 3.113981e-05 6.315763e-05
-# ENSG00000074582.12 0.04300055 4.193266e-05 5.586354e-05 1.112479e-03
-length(which(x[,3]<0.01))
-# [1] 391
+## choose the p range/cutoff of PRS has most correlation with expression
+lapply(colnames(COR),function(x){
+	length(which(COR[,x]<0.05))
+})
+# [[1]] 844 [[2]] 3552 [[3]] 3617 [[4]] 1608 [[5]] 658 [[6]] 626 [[7]] 573
+lapply(colnames(COR.adj),function(x){
+	length(which(COR.adj[,x]<0.05))
+})
+# 85 652 556 180 93 88 87
+## choose COR.adj
+save(COR,COR.adj,exp,prs,prs.res,file='/home/data1/R/prs/ed_n127/n125_pearsonCor_result.rda')
 
 
 ############################## 
@@ -99,19 +107,51 @@ library(clusterProfiler)
 geneUniverse = as.character(sigGeneOV$EntrezID)
 geneUniverse = geneUniverse[!is.na(geneUniverse)]
 
-id=which(COR$p[,3]<0.01)
+id=which(COR.adj[,2]<0.05)
 sig=rowData(rse_gene)[id,]
 sig=unique(as.character(sig$EntrezID[!is.na(sig$EntrezID)]))
 length(sig)
-# [1] 266
+# [1] 506
 
-moduleGeneList =list(sig=sig)
-goBP <- compareCluster(moduleGeneList, fun = "enrichGO",
-                       universe = geneUniverse, OrgDb = org.Hs.eg.db,
-                       ont = "BP", pAdjustMethod = "BH",
-                       pvalueCutoff  = 1, qvalueCutoff  = 1,
-                       readable= TRUE)
-# no significant term
+goBP=enrichGO(gene          = sig,
+               universe      = geneUniverse,
+               OrgDb         = org.Hs.eg.db,
+               ont           = "BP",
+               pAdjustMethod = "BH",
+               pvalueCutoff  = 0.1,
+               qvalueCutoff  = 0.5,
+               readable      = TRUE)
+goMF=enrichGO(gene          = sig,
+              universe      = geneUniverse,
+              OrgDb         = org.Hs.eg.db,
+              ont           = "MF",
+              pAdjustMethod = "BH",
+              pvalueCutoff  = 0.1,
+              qvalueCutoff  = 0.5,
+              readable      = TRUE)
+# no sig
+goCC=enrichGO(gene          = sig,
+              universe      = geneUniverse,
+              OrgDb         = org.Hs.eg.db,
+              ont           = "CC",
+              pAdjustMethod = "BH",
+              pvalueCutoff  = 0.1,
+              qvalueCutoff  = 0.5,
+              readable      = TRUE) 
+write_xlsx(list("BP"=goBP@result,"MF"=goMF@result,"CC"=goCC@result), 
+				'./20230816_p.adjless0.05_PRS_GO.xlsx')
 
-############# Group Separately ##################
-## eating disorder only  ###
+
+pdf('PRS exp correlation BP_GO.pdf',height=10,width=8)
+dotplot(goBP,showCategory = 20,title="Expression PRS correlation p.adj<0.05 Biological Process GO")
+dev.off()
+
+pdf('PRS exp correlation MF_GO.pdf',height=10,width=8)
+dotplot(goMF,showCategory = 20,title="Expression PRS correlation p.adj<0.05 Molecular Functions GO")
+dev.off()
+
+pdf('PRS exp correlation CC_GO.pdf',height=10,width=8)
+dotplot(goCC,showCategory = 20,title="Expression PRS correlation p.adj<0.05 Cellular Components GO")
+dev.off()
+
+save(COR,COR.adj,exp,prs,prs.res,goBP,goMF,goCC,file='/home/data1/R/prs/ed_n127/n125_pearsonCor_result.rda')
